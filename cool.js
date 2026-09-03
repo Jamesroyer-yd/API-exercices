64020aad2d6aae15ff8610df4bd67d8b3305461d
Jamesroyer-yd <royer.james.il@gmail.com>
2026-09-03 09:16:35 +0200

    Images pokemon implantées

--- cool.js
+++ cool.js
@@ -1,16 +1,28 @@
-// // 
-// EXERCICES - Contacter une API 
+// //
+// EXERCICES - Contacter une API
 // // Vous devez contacter l'api pokemon : https://pokeapi.co/ pour afficher les 20 premiers pokemon.
 
 async function display20pkm() {
     const reponse = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
     const donnees = await reponse.json();
-    donnees.results.forEach(pokemon => console.log(pokemon.name));
-    console.log(donnees.results[0].url);
-    const urlAPI = await fetch(donnees.results[0].url);
-    const tabpkm = await urlAPI.json();
-    console.log(tabpkm.sprites.front_default;
-    )
+
+    for (const pokemon of donnees.results) {
+        const urlAPI = await fetch(pokemon.url);
+        const tabpkm = await urlAPI.json();
+
+        const carte = document.createElement("div");
+
+        const image = document.createElement("img");
+        image.src = tabpkm.sprites.front_default;
+        image.alt = pokemon.name;
+        carte.appendChild(image);
+
+        const nom = document.createElement("p");
+        nom.textContent = pokemon.name;
+        carte.appendChild(nom);
+
+        document.body.appendChild(carte);
+    }
 }
 
 display20pkm();
